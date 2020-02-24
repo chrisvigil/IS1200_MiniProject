27524678fdd32900b57567b8bc6fd7e6d0901caf
Chris Vigil <chrisxvigil@gmail.com>
2020-02-24 11:33:10 +0100

    Added text to menu

--- stage0.c
+++ stage0.c
@@ -12,6 +12,7 @@ void new_frame_0(void)
     {
         frame[i] = 255;
     }
+    /*
     //S
     frame[181] = 0x47;
     frame[182] = 0x57;
@@ -36,6 +37,47 @@ void new_frame_0(void)
     frame[196] = 0xF7;
     frame[197] = 0x07;
     frame[198] = 0xF7;
+    */
+
+    //S
+    frame[172] = 0x31;
+    frame[173] = 0x20;
+    frame[174] = 0x24;
+    frame[175] = 0x24;
+    frame[176] = 0x04;
+    frame[177] = 0x8C;
+
+    //T
+    frame[180] = 0xFC;
+    frame[181] = 0xFC;
+    frame[182] = 0x00;
+    frame[183] = 0x00;
+    frame[184] = 0xFC;
+    frame[185] = 0xFC;
+
+    //A
+    frame[188] = 0x01;
+    frame[189] = 0x00;
+    frame[190] = 0xE4;
+    frame[191] = 0xE4;
+    frame[192] = 0x00;
+    frame[193] = 0x01;
+
+    //R
+    frame[196] = 0x00;
+    frame[197] = 0x00;
+    frame[198] = 0xE4;
+    frame[199] = 0xE4;
+    frame[200] = 0x00;
+    frame[201] = 0x19;
+
+    //T
+    frame[204] = 0xFC;
+    frame[205] = 0xFC;
+    frame[206] = 0x00;
+    frame[207] = 0x00;
+    frame[208] = 0xFC;
+    frame[209] = 0xFC;
 
 }
 
