d002e76f92b43f8eef6ce628e4d08044aa9be9e1
Chris Vigil <chrisxvigil@gmail.com>
2020-02-26 18:01:48 +0100

    more menu work

--- stage2.c
+++ stage2.c
@@ -19,11 +19,54 @@ void new_frame_2(void)
   drawletter('e',29,frame,3);
   drawletter('e',33,frame,3);
   drawletter('d',38,frame,3);
+
+  drawletter('s',75,frame,3);
+  drawletter('p',80,frame,3);
+  drawletter('a',85,frame,3);
+  drawletter('c',89,frame,3);
+  drawletter('i',93,frame,3);
+  drawletter('n',98,frame,3);
+  drawletter('g',103,frame,3);
+
+  drawletter('i',275+3,frame,3);
+  drawletter('n',280+3,frame,3);
+  drawletter('f',284+3,frame,3);
+  drawletter('o',289+3,frame,3);
+
+  drawletter('b',347-9,frame,3);
+  drawletter('a',352-9,frame,3);
+  drawletter('c',356-9,frame,3);
+  drawletter('k',361-9,frame,3);
 }
 
 void stage2_int(void)
 {
   new_frame_2();
+
+  int i;
+  switch(pos_2)
+  {
+    case 0:
+    for (i = 148; i < 171; i++)
+      frame[i] = 0xFD;
+    break;
+
+    case 1:
+    for (i = 203; i < 236; i++)
+      frame[i] = 0xFD;
+    break;
+
+    case 2:
+    for (i = 406; i < 425; i++)
+      frame[i] = 0xFD;
+    break;
+
+    case 3:
+    for (i = 466; i < 485; i++)
+      frame[i] = 0xFD;
+    break;
+
+  }
   display_image(frame);
 }
 
@@ -31,6 +74,8 @@ void stage2_int(void)
 void stage2_work(void)
 {
   int btnstate;
+  int btn3pushed = 0;
+
   while (stage == 2)
   {
     btnstate = getbtns();
@@ -45,7 +90,26 @@ void stage2_work(void)
           case 1:
             stage = 0;
             break;
+          case 2:
+            stage = 0;
+            break;
+          case 3:
+            stage = 0;
+            break;
         }
+    }
+
+    if ((btnstate & 4) && (btn3pushed <= 0) )
+    {
+      if (pos_2 < 3)
+        pos_2++;
+      else
+        pos_2 = 0;
+      btn3pushed = 100000;
+    }
+    else
+    {
+      btn3pushed--;
     }
   }
 }
