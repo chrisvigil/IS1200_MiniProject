fbe7eb0a458d64db466f9b31188035d3caddc5e3
Chris Vigil <chrisxvigil@gmail.com>
2020-02-27 13:14:18 +0100

    added files for stage 5 and 6

---
+++ stage5.c
@@ -0,0 +1,28 @@
+#include <stdint.h>   /* Declarations of uint_32 and the like */
+#include <pic32mx.h>
+#include "header.h"
+
+uint8_t frame[512];
+uint8_t pos_5 = 0;
+
+void new_frame_5(void)
+{
+  /* sets all pixels in frame to off */
+  int i = 0;
+  for (i = 0; i < 512; i++)
+  {
+      frame[i] = 255;
+  }
+}
+
+void stage5_int(void)
+{
+  new_frame_5();
+  display_image();
+}
+
+
+void stage5_work(void)
+{
+
+}
---
+++ stage6.c
@@ -0,0 +1,28 @@
+#include <stdint.h>   /* Declarations of uint_32 and the like */
+#include <pic32mx.h>
+#include "header.h"
+
+uint8_t frame[512];
+uint8_t pos_6 = 0;
+
+void new_frame_6(void)
+{
+  /* sets all pixels in frame to off */
+  int i = 0;
+  for (i = 0; i < 512; i++)
+  {
+      frame[i] = 255;
+  }
+}
+
+void stage6_int(void)
+{
+  new_frame_6();
+  display_image();
+}
+
+
+void stage6_work(void)
+{
+
+}
