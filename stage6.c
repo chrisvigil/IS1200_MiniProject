a7e3748bca13ff09ed7b9476d502825da34f824f
SaraBertse <sara.bertse@outlook.com>
2020-02-28 14:56:07 +0100

    Credits

--- stage6.c
+++ stage6.c
@@ -2,7 +2,7 @@
 #include <pic32mx.h>
 #include "header.h"
 
-//ABOUT stage
+//CREDITS stage
 
 uint8_t frame[512];
 uint8_t pos_6 = 0;
@@ -15,16 +15,46 @@ void new_frame_6(void)
   {
       frame[i] = 255;
   }
+
+  drawword("made", 20, frame, 3, 4);
+  drawword("by", 41, frame, 3, 2);
+  drawword("chris", 148, frame, 3, 5);
+  drawword("vigil", 175, frame, 3, 5);
+  drawword("and", 276, frame, 3, 3);
+  drawword("sara", 404, frame, 3, 4);
+  drawword("bertse", 426, frame, 3, 6);
 }
 
 void stage6_int(void)
 {
   new_frame_6();
-  display_image();
+  display_image(frame);
 }
 
 
 void stage6_work(void)
 {
+  int btnstate;
+  int btn3pushed = 0;
 
+  while (stage == 6)
+  {
+    btnstate = getbtns();
+    if (btnstate & 8) //Button to the furthest left
+    {
+      stage = 2;
+    }
+
+    if ((btnstate & 4) && (btn3pushed <= 0) )
+    {
+      if (pos_6 = 0)
+        pos_6 = 1;
+      else
+      btn3pushed = 100000;
+    }
+    else
+    {
+      btn3pushed--;
+    }
+  }
 }
